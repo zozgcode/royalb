f94d3f78b0f40d95ce92caad24dd0a462dacb860
sunshine-web-coder <webcoderdev02@gmail.com>
2023-09-04 22:24:34 +0100

    first commit

---
+++ src/components/userDB.jsx
@@ -0,0 +1,11 @@
+const users = [
+  { 
+    id: 1,
+    name: "Samuel D Marcus",
+    cardName: "Samuel D. Marcus",
+    username: "Samuel2341",
+    password: "Spiderman@1992",
+    amount: "3,293,950.19",
+  },
+];
+export default users;
